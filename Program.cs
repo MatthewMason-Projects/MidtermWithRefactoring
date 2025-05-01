80a9c0af7c274135b43e03200852225f8751dca3
MatthewMason-Projects <mattmason0727@gmail.com>
2025-05-01 03:04:36 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -16,3 +16,9 @@ namespace ATMApp
         }
     }
 }
+//Unit testing is the process of testing individual components or methods by themselves to make sure they work as they should.
+// It helps developers catch bugs early in the development process.
+//By writing tests for critical methods such as login authentication, balance checks, or withdrawal logic, developers can make sure the code works immediately and modularly.
+//Unit tests support refactoring because it helps test one thing at a time.
+//They also serve as a form of documentation, helping future developers understand what the code should be doing.
+//Automated unit tests can speed up the development cycle and improve quality.
